2b0c427497a401ca9a9b628fde1d7eca93ba68aa
Roger <thrasher.bis@gmail.com>
2018-02-06 12:58:42 +0100

    jj

--- src/main/java/salvo/salvo/BattleshipApplication.java
+++ src/main/java/salvo/salvo/BattleshipApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class BattleshipApplication {
 
@@ -14,7 +18,7 @@ public class BattleshipApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(PlayerRepository playerRepository, GameRepository gameRepository,  GamePlayerRepository gamePlayerRepository) {
+	public CommandLineRunner initData(ShipRepository shipRepository, PlayerRepository playerRepository, GameRepository gameRepository,  GamePlayerRepository gamePlayerRepository) {
 		return (args) -> {
 
 			// We create the players
@@ -55,6 +59,40 @@ public class BattleshipApplication {
 			gamePlayerRepository.save(gamePlayer3);
 			gamePlayerRepository.save(gamePlayer4);
 			gamePlayerRepository.save(gamePlayer5);
+
+			//We create the locations
+			List<String> locations5 = Arrays.asList("A2","A3","A4","A5","A6");
+			List<String> locations4 = Arrays.asList("B5","C5","D5","E5");
+			List<String> locations2 = Arrays.asList("C6","C7");
+			List<String> locations22 = Arrays.asList("B6","B7");
+
+			//We create the ships only with type, we then later assign the gameplayers and locations
+			Ship Carrier = new Ship("carrier");
+			Ship Battleship = new Ship("battleship");
+			Ship Submarine = new Ship("submarine");
+			Ship PatrolBoat = new Ship("patrol boat");
+			Ship Destroyer = new Ship("destroyer");
+
+			Carrier.setGamePlayer(gamePlayer1);
+			Battleship.setGamePlayer(gamePlayer1);
+			Submarine.setGamePlayer(gamePlayer3);
+			PatrolBoat.setGamePlayer(gamePlayer4);
+			Destroyer.setGamePlayer(gamePlayer5);
+
+			Carrier.setLocations(locations5);
+			Battleship.setLocations(locations4);
+			Submarine.setLocations(locations2);
+			PatrolBoat.setLocations(locations22);
+			Destroyer.setLocations(locations22);
+
+
+			//We save the ships
+			shipRepository.save(Carrier);
+			shipRepository.save(Battleship);
+			shipRepository.save(Submarine);
+			shipRepository.save(PatrolBoat);
+			shipRepository.save(Destroyer);
+
 		};
 
 	}
